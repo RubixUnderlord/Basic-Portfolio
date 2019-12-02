37c632028c69d1af12bf12c70aa24ebe382c0137
RubixUnderlord <57545547+RubixUnderlord@users.noreply.github.com>
2019-12-02 14:42:03 -0700

    contact working

---
+++ action_page.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $Name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $Message = $_POST['Message'];
+
+    $mailTo = "robbysaldi@yahoo.com";
+    $subject = "Website Stuff";
+    $headers = "From: ".$Email;
+    $txt = "You have received an e-mail from".$Name.".\n\n".$Message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
